b2659a5701fa0d094e7b5726a5c5eab24221ea25
Saborowski, Kai <kai.saborowski@hdi.de>
2024-11-20 15:26:43 +0100

    Added CI worflow, added Test for Producer Application.

---
+++ logmessage-producer/src/test/java/de/ksbrwsk/logback/ScheduledMessageProducerTest.java
@@ -0,0 +1,54 @@
+package de.ksbrwsk.logback;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.awaitility.Awaitility.waitAtMost;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Slf4j
+class ScheduledMessageProducerTest {
+
+    @Autowired
+    TestListener testListener;
+
+    @Test
+    void runningScheduledMessageProducerFor5Sec() {
+        log.info("*** Create and run ScheduledMessageProducer");
+        ScheduledMessageProducer producer = new ScheduledMessageProducer();
+        producer.createMessage();
+        waitAtMost(Duration.ofSeconds(5))
+                .untilAsserted(() -> {
+                    log.info("*** Test running for 5 sec., received messages: {}", this.testListener.messages.size());
+                    assertTrue(this.testListener.messages.size() > 0);
+                });
+    }
+
+    @TestConfiguration
+    static class Config {
+
+        @Bean
+        TestListener testListener() {
+            return new TestListener();
+        }
+    }
+
+    static class TestListener {
+        private final List<String> messages = new ArrayList<>();
+
+        @RabbitListener(queues = "queue-logs")
+        void listen(String data) {
+            this.messages.add(data);
+        }
+    }
+}
---
+++ logmessage-producer/src/test/java/de/ksbrwsk/logback/TestcontainersConfiguration.java
@@ -0,0 +1,16 @@
+package de.ksbrwsk.logback;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.RabbitMQContainer;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    RabbitMQContainer rabbitMQContainer() {
+        return new RabbitMQContainer("rabbitmq:4.0.3-alpine");
+    }
+}
